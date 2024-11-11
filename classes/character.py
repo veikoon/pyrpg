7f6b90edd7e7e1b28dfdc6191c2a4c4f791e90e4
Vincent MONNOT <barzul94@gmail.com>
2024-11-11 17:59:22 +0100

    firsts ut

--- classes/character.py
+++ classes/character.py
@@ -7,7 +7,9 @@ from .item import Item, Weapon, Armor
 class Inventory:
     """Inventory class"""
 
-    items : dict[str:Item] = None
+    items       : dict[str:Item] = None
+    max_size    : int = 40
+
 
     def __post_init__(self):
 
@@ -22,17 +24,46 @@ class Inventory:
         """Get the value sum of all item in inventory"""
         return sum(i.get_value() for i in self.items.values())
 
+    def get_size(self) -> int:
+        """Get the quentity of item"""
+        return sum(i.quentity for i in self.items.values())
+
     def add_item(self, item: Item) -> dict[str:Item]:
         """Add an item to the inventory"""
-        if item.name in self.items:
+        if item.id in self.items:
             self.items[item.id].quentity += item.quentity
         else:
             self.items[item.id] = item
+        return self.items
 
-    def add_items(self, items : list[Item]) -> dict[str:Item]:
+    def add_items(self, items: list[Item]) -> dict[str:Item]:
         """Add multiple item"""
         for i in items:
             self.add_item(i)
+        return self.items
+
+    def get_item(self, id: str) -> Item:
+        """Return item from an id"""
+        return self.items[id]
+
+    def search_item(self, name: str) -> list[Item]:
+        """Return item from a name"""
+        return [i for i in self.items.values() if i.name == name]
+
+    def remove_item(self, item_id: str, quentity: int=1) -> dict[str:Item]:
+        """Remove an item from the inventory"""
+        if item_id in self.items:
+            item = self.get_item(item_id)
+            item.quentity -= quentity
+            if item.quentity <= 0:
+                del self.items[item_id]
+        return self.items
+
+    def remove_items(self, items: list[str]) -> dict[str:Item]:
+        """Remove multiple item"""
+        for i in items:
+            self.remove_item(i)
+        return self.items
 
 @dataclass
 class Caracteristics:
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 """
 Main file
 """
-from tests import print_player, pickup_item
 
 def main():
     """Main class"""
-    print_player()
-    pickup_item()
 
 if __name__ == "__main__":
     main()
